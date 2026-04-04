c78a64bca21acfbc97a98a0c8e46ce3b554890c9
HM-Atalan <hilmimertatalan@gmail.com>
2026-04-04 03:53:34 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
 
-import { getAuth, onAuthStateChanged, signInWithPopup, signInWithEmailAndPassword,
-         createUserWithEmailAndPassword, GoogleAuthProvider, signOut } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+import { getAuth, onAuthStateChanged, signInWithPopup, signInWithEmailAndPassword,createUserWithEmailAndPassword, GoogleAuthProvider, signOut } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 
 import { getRemoteConfig, getValue, fetchAndActivate } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-remote-config.js';
 
